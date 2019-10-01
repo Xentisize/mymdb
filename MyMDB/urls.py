d11d6c336a715b4f9ae82a67bbd548df83d2a23f
Xens Chan <xentisize@gmail.com>
2019-10-01 23:20:58 +0800

    P.60 Working on user

--- MyMDB/urls.py
+++ MyMDB/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("user/", include("user.urls", namespace="user")),
     path("", include("core.urls", namespace="core")),
 ]
 
